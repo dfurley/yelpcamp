32799f14214970e49341db87f04748a0c8c15911
David Furley <furley.david@gmail.com>
2018-02-28 17:05:22 +0000

    Added mongo DB for data persistence

--- app.js
+++ app.js
@@ -1,23 +1,49 @@
 var express = require("express");
 var app = express();
 var bodyParser = require("body-parser");
+var mongoose = require("mongoose");
 
+mongoose.connect("mongodb://localhost/yelp_camp");
 app.use(bodyParser.urlencoded({extended:true}));
 app.set("view engine", "ejs");
 
-var campgrounds = [
-        {name: "Salmon Creek", image: "https://images.pexels.com/photos/699558/pexels-photo-699558.jpeg?h=350&auto=compress&cs=tinysrgb"},
-        {name: "Granite Hill", image: "https://images.pexels.com/photos/93858/pexels-photo-93858.jpeg?h=350&auto=compress&cs=tinysrgb"},
-        {name: "Tunnel Mountain", image: "https://images.pexels.com/photos/216676/pexels-photo-216676.jpeg?h=350&auto=compress&cs=tinysrgb"},
-        {name: "Edale Campground", image: "https://images.pexels.com/photos/618848/pexels-photo-618848.jpeg?h=350&auto=compress&cs=tinysrgb"}
-    ];
+// SCHEMA SETUP
+var campgroundSchema = new mongoose.Schema({
+    name: String,
+    image: String
+});
+
+var Campground = mongoose.model("Campground", campgroundSchema);
+
+// TEMPORARY
+
+// Campground.create(
+//     {
+//         name: "Tunnel Mountain", 
+//         image: "https://images.pexels.com/photos/216676/pexels-photo-216676.jpeg?h=350&auto=compress&cs=tinysrgb"
+        
+//     }, function(err, campground) {
+//         if(err) {
+//             console.log(err);
+//         } else {
+//             console.log("NEW CAMPGROUND CREATED: ");
+//             console.log(campground);
+//         }
+//     });
 
 app.get("/", function(req, res){
     res.render("landing");
 });
 
 app.get("/campgrounds", function(req, res){
-    res.render("campgrounds",  {campgrounds: campgrounds});
+    // Get all campgrounds from DB
+    Campground.find({}, function(err, allCampgrounds){
+       if(err){
+           console.log(err);
+       } else {
+           res.render("campgrounds",  {campgrounds: allCampgrounds});
+       }
+    });
 });
 
 app.get("/campgrounds/new", function(req, res){
@@ -28,9 +54,15 @@ app.post("/campgrounds", function(req, res){
     var name = req.body.name;
     var image = req.body.image;
     var newCampground = {name:name, image:image};
-    campgrounds.push(newCampground);
-    
-    res.redirect("/campgrounds");
+    // create new campground and save to database
+    Campground.create(newCampground, function(err, newlyCreated){
+       if(err){
+           console.log(err);
+       } else {
+           // redirect back to campgrounds page
+           res.redirect("/campgrounds");
+       }
+    });
 });
 
 app.listen(process.env.PORT, process.env.IP, function(){
