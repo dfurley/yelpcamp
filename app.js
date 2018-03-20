ab28d2fd200487e5fab95e52eeb8ec138742f217
David Furley <furley.david@gmail.com>
2018-03-20 13:22:03 +0000

    added authentication

--- app.js
+++ app.js
@@ -3,8 +3,11 @@ var express = require("express"),
     app = express(),
     bodyParser = require("body-parser"),
     mongoose = require("mongoose"),
+    passport = require("passport"),
+    LocalStrategy = require("passport-local"),
     Campground = require("./models/campground"),
     Comment = require("./models/comment"),
+    User = require("./models/user"),
     seedDB = require("./seeds")
 
 // Database set up etc.
@@ -14,11 +17,36 @@ app.set("view engine", "ejs");
 app.use(express.static(__dirname + "/public"));
 seedDB();
 
+// Passport config
+app.use(require("express-session")({
+    secret: "A surprise to be sure, but a welcome one",
+    resave: false,
+    saveUninitialized: false
+}));
+app.use(passport.initialize());
+app.use(passport.session());
+passport.use(new LocalStrategy(User.authenticate()));
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
+
+// middleware to pass user data to every template
+app.use(function(req, res, next){
+   res.locals.currentUser = req.user;
+   next();
+});
+
+// ================
+// Routes
+// ================
 
 // Landing page
 app.get("/", function(req, res){
     res.render("landing");
 });
+
+// ==================
+// Campground Routes
+// ==================
 
 // INDEX - show all campgrounds
 app.get("/campgrounds", function(req, res){
@@ -27,7 +55,7 @@ app.get("/campgrounds", function(req, res){
        if(err){
            console.log(err);
        } else {
-           res.render("campgrounds/index",  {campgrounds: allCampgrounds});
+           res.render("campgrounds/index",  {campgrounds: allCampgrounds, currentUser: req.user});
        }
     });
 });
@@ -72,7 +100,7 @@ app.get("/campgrounds/:id", function(req, res){
 // =================
 
 // NEW
-app.get("/campgrounds/:id/comments/new", function(req, res){
+app.get("/campgrounds/:id/comments/new", isLoggedIn, function(req, res){
    // find campground by id
    Campground.findById(req.params.id, function(err, campground){
       if(err){
@@ -84,7 +112,7 @@ app.get("/campgrounds/:id/comments/new", function(req, res){
 });
 
 // CREATE
-app.post("/campgrounds/:id/comments", function(req, res){
+app.post("/campgrounds/:id/comments", isLoggedIn, function(req, res){
     //lookup campground using id
     Campground.findById(req.params.id, function(err, campground){
         if(err){
@@ -106,6 +134,57 @@ app.post("/campgrounds/:id/comments", function(req, res){
         }
     });
 });
+
+// ==================
+// Auth Routes
+// ==================
+
+// SHOW
+app.get("/register", function(req, res){
+    res.render("auth/register")
+});
+
+// handle sign up logic
+app.post("/register", function(req, res){
+    // create new user and save to newUser variable
+    var newUser = new User({username: req.body.username});
+    // pass newUser and password to passport, if error redirect back to register, else save user to db and redirect to campgrounds page
+    User.register(newUser, req.body.password, function(err, user){
+       if(err){
+           console.log(err);
+           return res.render("register")
+       }
+       passport.authenticate("local")(req, res, function(){
+           res.redirect("/campgrounds");
+       });
+    });
+});
+
+// show login form
+app.get("/login", function(req, res){
+    res.render("auth/login");
+});
+// handling login logic - using passport.authenticate middleware
+app.post("/login", passport.authenticate("local", 
+    {
+        successRedirect: "/campgrounds",
+        failureRedirect: "/login"
+    }), function(req, res){
+});
+
+// logout logic
+app.get("/logout", function(req, res){
+    req.logout();
+    res.redirect("/");
+});
+
+// middleware to check is user is logged in 
+function isLoggedIn(req, res, next){
+    if(req.isAuthenticated()){
+        return next();
+    }
+    res.redirect("/login");
+}
 
 // Launch server
 app.listen(process.env.PORT, process.env.IP, function(){
