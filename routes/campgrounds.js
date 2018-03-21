3d5eff68b49b8ff94700ee4a5195d91207ecc83c
David Furley <furley.david@gmail.com>
2018-03-21 16:14:16 +0000

    added user association for campgrounds and comments

--- routes/campgrounds.js
+++ routes/campgrounds.js
@@ -19,16 +19,20 @@ router.get("/", function(req, res){
 });
 
 // NEW - show form to create new campground
-router.get("/new", function(req, res){
+router.get("/new", isLoggedIn, function(req, res){
    res.render("campgrounds/new"); 
 });
 
 // CREATE - add new campground to database
-router.post("/", function(req, res){
+router.post("/", isLoggedIn ,function(req, res){
     var name = req.body.name;
     var image = req.body.image;
     var description = req.body.description;
-    var newCampground = {name:name, image:image, description:description};
+    var author = {
+        id: req.user._id,
+        username: req.user.username
+    }
+    var newCampground = {name:name, image:image, description:description, author:author};
     // create new campground and save to database
     Campground.create(newCampground, function(err, newlyCreated){
        if(err){
